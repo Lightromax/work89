d1559eb53219f57dc708d8873be542f94a7c93a6
Lightromax-Jetlearn <calendara132@gmail.com>
2025-09-06 17:00:16 +0100

    Add files via upload

--- flappyball.py
+++ flappyball.py
@@ -5,7 +5,7 @@ pygame.init()
 WIDTH=600
 HEIGHT=600
 
-screen=pygame.display.set_mode((WIDTH,HEIGHT))
+screen=pygame.display.set_mode((WIDTH,HEIGHT),pygame.RESIZABLE)
 pygame.display.set_caption("Flappy Ball")
 timer=pygame.time.Clock()
 fps=60
@@ -29,7 +29,13 @@ run=True
 
 while(run):
     screen.fill(lightblue)
+    ball=pygame.draw.circle(screen,green,(ballx,ballY),90,0)
     for events in pygame.event.get():
         if events.type == pygame.QUIT:
             run=False
             pygame.quit()
+        if events.type == pygame.KEYDOWN:
+            if pygame.key == pygame.K_UP:
+                ballvx = bounce
+    ballvx += gravity
+    ballY += ballvx
